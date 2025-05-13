9a18e9b4b659e1e87541cba9cd12b121c5e1e4b8
Erbey Uribe <erbey.uribe@gmail.com>
2025-05-13 12:32:43 -0500

    added comments to code

--- api_services/src/api_handler.py
+++ api_services/src/api_handler.py
@@ -1,14 +1,18 @@
+# uses FastApi - see documentation at https://fastapi.tiangolo.com/
+
 import glob
 import os
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 
+# returns the latest image assuming folder structure hasn't changed
 def get_latest_image():
     file = glob.iglob("../../src/rs_timer/rs-save-*.png")
     return max(file, key=os.path.getmtime, default=None)
 
 app = FastAPI()
 
+# get request to get latest image
 @app.get("/", response_class=FileResponse)
 async def main():
     image = get_latest_image()
--- api_services/src/file_handler.py
+++ api_services/src/file_handler.py
@@ -1,15 +1,19 @@
+# watchdog that keeps only the five most recent files
+
 import glob
 import os
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 path = "../../src/rs_timer/"
+file_num = 5;
 
+# tiggers on file creation to keep the most recent FILE_NUM files
 class FileCreatedEventHandler(FileSystemEventHandler):
     def on_created(self, event):
         file = glob.glob(os.path.join(path, "rs-save-*.png"))
         file.sort(key=os.path.getmtime)
-        for f in file[:-5]:
+        for f in file[:-file_num]:
             os.remove(f)
 
 event_handler = FileCreatedEventHandler()
